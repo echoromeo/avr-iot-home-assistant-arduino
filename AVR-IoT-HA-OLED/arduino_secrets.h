940179c8dd0552bd8e13bf35fd0abda174603cb8
Egil Rotevatn <Egil.Rotevatn@Microchip.com>
2024-10-22 14:55:48 +0200

    Removed the MAC secret

--- AVR-IoT-HA-OLED/arduino_secrets.h
+++ AVR-IoT-HA-OLED/arduino_secrets.h
@@ -3,7 +3,6 @@
 
 #define SECRET_SSID   ""
 #define SECRET_PASS   ""
-#define SECRET_MAC    {0x00, 0x10, 0xFA, 0x6E, 0x38, 0x4A}
 
 #define SECRET_BROKER    IPAddress(192,168,0,17)
 #define SECRET_HA_USER   ""
